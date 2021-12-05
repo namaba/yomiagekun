7fd3155025f775ed6a4cc8420b82b8c3d8049ed3
namaba <one.2426@gmail.com>
2021-12-06 01:03:03 +0900

    feat: テキストエリアを追加し、入力したテキスト文読まれるようにした

--- src/app.tsx
+++ src/app.tsx
@@ -1,65 +1,35 @@
+import { useTextToSpeak } from './hooks/useTextToSpeak'
+import './index.css'
+
 export function App() {
-  const speak = (text: string) => {
-    const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY
-    console.log(import.meta.env.VITE_GOOGLE_API_KEY)
-    const url =
-      'https://texttospeech.googleapis.com/v1/text:synthesize?key=' +
-      googleApiKey
-    const data = {
-      input: {
-        text: text,
-      },
-      voice: {
-        languageCode: 'ja-JP',
-        name: 'ja-JP-Standard-B',
-      },
-      audioConfig: {
-        audioEncoding: 'MP3',
-        speaking_rate: '1.00',
-        pitch: '0.00',
-      },
-    }
-    const otherparam = {
-      headers: {
-        'content-type': 'application/json; charset=UTF-8',
-      },
-      body: JSON.stringify(data),
-      method: 'POST',
-    }
-    fetch(url, otherparam)
-      .then((data) => {
-        return data.json()
-      })
-      .then((res) => {
-        try {
-          var blobUrl = base64ToBlobUrl(res.audioContent)
-          var audio = new Audio()
-          audio.src = blobUrl
-          audio.play()
-        } catch (e) {
-          console.log(e)
-        }
-      })
-      .catch((error) => alert(error))
-  }
+  const { value, handleChange, handleSubmit } = useTextToSpeak<{}>()
 
-  // Base6z4 → BlobUrl
-  function base64ToBlobUrl(base64: string) {
-    var bin = atob(base64.replace(/^.*,/, ''))
-    var buffer = new Uint8Array(bin.length)
-    for (var i = 0; i < bin.length; i++) {
-      buffer[i] = bin.charCodeAt(i)
-    }
-    return window.URL.createObjectURL(
-      new Blob([buffer.buffer], { type: 'audio/wav' })
-    )
-  }
-
-  console.log(import.meta.env.VITE_API_KEY)
   return (
     <div>
-      <h1 className="font-bold text-4xl">yomiagekunだよ</h1>
-      <button onClick={() => speak('おはようございます')}>音声合成</button>
+      <h1 className="text-purple-600 font-bold text-2xl">yomiagekunだよ</h1>
+      <form onSubmit={handleSubmit}>
+        <label className="block">
+          <span className="text-gray-700">読み上げるテキスト</span>
+          <textarea
+            className="
+          mt-0
+          block
+          w-full
+          px-0.5
+          border-0 border-b-2 border-gray-200
+          focus:ring-0 focus:border-black"
+            value={value}
+            onChange={handleChange}
+          />
+        </label>
+        <button
+          type="submit"
+          value="Submit"
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+        >
+          送信
+        </button>
+      </form>
     </div>
   )
 }
