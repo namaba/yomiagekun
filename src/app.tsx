cc7f55cf7a56b890e4a9ed41f3c184aa28910b31
namaba <one.2426@gmail.com>
2021-12-05 22:30:24 +0900

    feat: text-to-speechAPIのつなぎこみ

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,64 @@
 export function App() {
+  const speak = (text: string) => {
+    const googleApiKey = import.meta.env.VITE_API_KEY
+    const url =
+      'https://texttospeech.googleapis.com/v1/text:synthesize?key=' +
+      googleApiKey
+    const data = {
+      input: {
+        text: text,
+      },
+      voice: {
+        languageCode: 'ja-JP',
+        name: 'ja-JP-Standard-B',
+      },
+      audioConfig: {
+        audioEncoding: 'MP3',
+        speaking_rate: '1.00',
+        pitch: '0.00',
+      },
+    }
+    const otherparam = {
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(data),
+      method: 'POST',
+    }
+    fetch(url, otherparam)
+      .then((data) => {
+        return data.json()
+      })
+      .then((res) => {
+        try {
+          var blobUrl = base64ToBlobUrl(res.audioContent)
+          var audio = new Audio()
+          audio.src = blobUrl
+          audio.play()
+        } catch (e) {
+          console.log(e)
+        }
+      })
+      .catch((error) => alert(error))
+  }
+
+  // Base6z4 → BlobUrl
+  function base64ToBlobUrl(base64: string) {
+    var bin = atob(base64.replace(/^.*,/, ''))
+    var buffer = new Uint8Array(bin.length)
+    for (var i = 0; i < bin.length; i++) {
+      buffer[i] = bin.charCodeAt(i)
+    }
+    return window.URL.createObjectURL(
+      new Blob([buffer.buffer], { type: 'audio/wav' })
+    )
+  }
+
+  console.log(import.meta.env.VITE_API_KEY)
   return (
     <div>
-      <h1 className="font-bold text-4xl">yomiagekun</h1>
+      <h1 className="font-bold text-4xl">yomiagekunだよ</h1>
+      <button onClick={() => speak('おはようございます')}>音声合成</button>
     </div>
   )
 }
